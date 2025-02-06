4fc04e0bf9ec7f1d87a348c8131ccfc5e8aa8651
Chamil José Cruz Razeq (AIDA) <chcruz@gda.dis>
2025-02-06 13:48:51 +0000

    TennisGame5 refactor

--- src/TennisRefactoringKata/TennisGame5.cs
+++ src/TennisRefactoringKata/TennisGame5.cs
@@ -1,4 +1,6 @@
-﻿namespace TennisRefactoringKata
+﻿using System.ComponentModel.Design;
+
+namespace TennisRefactoringKata
 {
     public class TennisGame5 : ITennisGame
     {
@@ -15,53 +17,77 @@
 
         public void WonPoint(string playerName)
         {
-            if (playerName == player1Name)
-                player1Score++;
-            else if (playerName == player2Name)
-                player2Score++;
-            else
+            var isNotPlayer = (playerName == player1Name) && (playerName == player2Name);
+
+            if (isNotPlayer) {
                 throw new ArgumentException("Invalid player name.");
+            }
+
+            if (playerName == player1Name)
+            {
+                player1Score++;
+            } else {
+                player2Score++;
+            }
         }
 
         public string GetScore()
         {
-            int p1 = player1Score;
-            int p2 = player2Score;
+            int player1Score = this.player1Score;
+            int player2Score = this.player2Score;
 
-            while (p1 > 4 || p2 > 4)
+            while (player1Score > 4 || player2Score > 4)
             {
-                p1--;
-                p2--;
+                player1Score--;
+                player2Score--;
             }
 
-            return (p1, p2) switch
+            return MatchStateToString(player1Score, player2Score);
+        }
+
+        private string MatchStateToString(int player1Score, int player2Score)
+        {
+            var isNotEndGame = ((player1Score + player2Score) < 6) && ((player1Score != 4) && (player2Score != 4));
+            
+            if (isNotEndGame)
             {
-                (0, 0) => "Love-All",
-                (0, 1) => "Love-Fifteen",
-                (0, 2) => "Love-Thirty",
-                (0, 3) => "Love-Forty",
-                (0, 4) => $"Win for {player2Name}",
-                (1, 0) => "Fifteen-Love",
-                (1, 1) => "Fifteen-All",
-                (1, 2) => "Fifteen-Thirty",
-                (1, 3) => "Fifteen-Forty",
-                (1, 4) => $"Win for {player2Name}",
-                (2, 0) => "Thirty-Love",
-                (2, 1) => "Thirty-Fifteen",
-                (2, 2) => "Thirty-All",
-                (2, 3) => "Thirty-Forty",
-                (2, 4) => $"Win for {player2Name}",
-                (3, 0) => "Forty-Love",
-                (3, 1) => "Forty-Fifteen",
-                (3, 2) => "Forty-Thirty",
-                (3, 3) => "Deuce",
-                (3, 4) => $"Advantage {player2Name}",
-                (4, 0) => $"Win for {player1Name}",
-                (4, 1) => $"Win for {player1Name}",
-                (4, 2) => $"Win for {player1Name}",
-                (4, 3) => $"Advantage {player1Name}",
-                (4, 4) => "Deuce",
-                _ => throw new ArgumentException("Invalid score.")
+                if (player1Score == player2Score)
+                {
+                    return $"{ScoreSlang(player1Score)}-All";
+                }
+                else
+                {
+                    return $"{ScoreSlang(player1Score)}-{ScoreSlang(player2Score)}";
+                }
+            } 
+            else
+            {
+                var leadingPlayer = player1Score > player2Score ? player1Name : player2Name;
+                var advantageCheck = (player1Score - player2Score) * (player1Score - player2Score) > 1;
+
+                if (player1Score == player2Score)
+                {
+                    return "Deuce"; 
+                }
+                else if (advantageCheck)
+                {
+                    return $"Win for {leadingPlayer}";
+                }
+                else
+                {
+                    return $"Advantage {leadingPlayer}";
+                }
+            }
+        }
+
+        public string ScoreSlang(int score) {
+            return score switch
+            {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => throw new ArgumentException("Invalid score."),
             };
         }
     }
