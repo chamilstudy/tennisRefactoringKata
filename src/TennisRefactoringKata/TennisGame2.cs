12ce0dcfb524d4f7ffd09483b65486b8a8fb9109
Chamil José Cruz Razeq (AIDA) <chcruz@gda.dis>
2025-02-06 10:50:51 +0000

    Game2: removed redundancies, extracted code into methods and reformat conditions

--- src/TennisRefactoringKata/TennisGame2.cs
+++ src/TennisRefactoringKata/TennisGame2.cs
@@ -2,141 +2,101 @@
 {
     public class TennisGame2 : ITennisGame
     {
-        private int p1point;
-        private int p2point;
+        private int player1Score;
+        private int player2Score;
+        private string player1SlangScore = "";
+        private string player2SlangScore = "";
 
-        private string p1res = "";
-        private string p2res = "";
         private string player1Name;
         private string player2Name;
 
         public TennisGame2(string player1Name, string player2Name)
         {
             this.player1Name = player1Name;
-            p1point = 0;
             this.player2Name = player2Name;
+            player1Score = player2Score = 0;
         }
 
         public string GetScore()
         {
-            var score = "";
-            if (p1point == p2point && p1point < 3)
+            if (player1Score == player2Score)
             {
-                if (p1point == 0)
-                    score = "Love";
-                if (p1point == 1)
-                    score = "Fifteen";
-                if (p1point == 2)
-                    score = "Thirty";
-                score += "-All";
+                return TieToTieSlang(player1Score);
             }
-            if (p1point == p2point && p1point > 2)
-                score = "Deuce";
-
-            if (p1point > 0 && p2point == 0)
+            else if (player1Score >= 4 || player2Score >= 4)
             {
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-
-                p2res = "Love";
-                score = p1res + "-" + p2res;
+                return CheckMatchState();
             }
-            if (p2point > 0 && p1point == 0)
+            else
             {
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-
-                p1res = "Love";
-                score = p1res + "-" + p2res;
+                player1SlangScore = ScoreToSlangScore(player1Score);
+                player2SlangScore = ScoreToSlangScore(player2Score);
+                return player1SlangScore + "-" + player2SlangScore;
             }
-
-            if (p1point > p2point && p1point < 4)
-            {
-                if (p1point == 2)
-                    p1res = "Thirty";
-                if (p1point == 3)
-                    p1res = "Forty";
-                if (p2point == 1)
-                    p2res = "Fifteen";
-                if (p2point == 2)
-                    p2res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-            if (p2point > p1point && p2point < 4)
-            {
-                if (p2point == 2)
-                    p2res = "Thirty";
-                if (p2point == 3)
-                    p2res = "Forty";
-                if (p1point == 1)
-                    p1res = "Fifteen";
-                if (p1point == 2)
-                    p1res = "Thirty";
-                score = p1res + "-" + p2res;
-            }
-
-            if (p1point > p2point && p2point >= 3)
-            {
-                score = "Advantage player1";
-            }
-
-            if (p2point > p1point && p1point >= 3)
-            {
-                score = "Advantage player2";
-            }
-
-            if (p1point >= 4 && p2point >= 0 && (p1point - p2point) >= 2)
-            {
-                score = "Win for player1";
-            }
-            if (p2point >= 4 && p1point >= 0 && (p2point - p1point) >= 2)
-            {
-                score = "Win for player2";
-            }
-            return score;
         }
 
-        public void SetP1Score(int number)
+        private string CheckMatchState()
+        {
+
+            var playerAdvantage = player1Score - player2Score;
+
+            switch (playerAdvantage) {
+                case 1:
+                    return "Advantage player1";
+                case -1:
+                    return "Advantage player2";
+                case >=2:
+                    return "Win for player1";
+                case <=2:
+                    return "Win for player2";
+            }
+        }
+
+        private string TieToTieSlang(int playerScore)
+        {
+            return playerScore switch
+            {
+                0 => "Love-All",
+                1 => "Fifteen-All",
+                2 => "Thirty-All",
+                _ => "Deuce",
+            };
+        }
+
+        private string ScoreToSlangScore(int playerScore)
+        {
+            return playerScore switch
+            {
+                0 => "Love",
+                1 => "Fifteen",
+                2 => "Thirty",
+                3 => "Forty",
+                _ => "",
+            };
+        }
+
+        public void SetPlayer1Score(int number)
         {
             for (int i = 0; i < number; i++)
             {
-                P1Score();
+                player1Score++;
             }
         }
 
-        public void SetP2Score(int number)
+        public void SetPlayer2Score(int number)
         {
             for (var i = 0; i < number; i++)
             {
-                P2Score();
+                player2Score++;
             }
-        }
-
-        private void P1Score()
-        {
-            p1point++;
-        }
-
-        private void P2Score()
-        {
-            p2point++;
         }
 
         public void WonPoint(string player)
         {
             if (player == "player1")
-                P1Score();
+                player1Score++;
             else
-                P2Score();
+                player2Score++;
         }
-
     }
 }
